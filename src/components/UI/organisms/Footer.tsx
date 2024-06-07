40986502c96ea4eaa7dc61d4b5d13e440a049744
Mars375 <loic_2610@hotmail.fr>
2024-06-07 14:42:49 +0200

    style: agrandir le logo HRnet dans le header

--- src/components/UI/organisms/Footer.tsx
+++ src/components/UI/organisms/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer: React.FC = () => {
 	return (
-		<footer className='bg-white dark:bg-gray-900 text-gray-900 dark:text-gray-100 text-center p-4 flex justify-center items-center border-t border-gray-200 dark:border-gray-700'>
+		<footer className='bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-100 text-center p-4 flex justify-center items-center border-t border-gray-200 dark:border-gray-700'>
 			<p className='text-gray-700 dark:text-gray-300'>
 				© 2023 Company Name. All rights reserved.
 			</p>
