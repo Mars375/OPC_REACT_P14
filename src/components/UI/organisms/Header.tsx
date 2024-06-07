eb2938278985276635a7c140c05480764d46043b
Mars375 <loic_2610@hotmail.fr>
2024-06-07 14:46:26 +0200

    feat: ajouter un lien autour du logo pour rediriger vers la page d'accueil

--- src/components/UI/organisms/Header.tsx
+++ src/components/UI/organisms/Header.tsx
@@ -6,11 +6,13 @@ const Header: React.FC = () => {
 	return (
 		<header className='bg-white dark:bg-gray-900 text-gray-900 dark:text-gray-100 p-4 flex justify-between items-center shadow-md'>
 			<div className='flex items-center space-x-4'>
-				<img
-					src='/HRLogo-removebg.png'
-					alt='HRnet Logo'
-					className='h-12 w-12'
-				/>
+				<Link to='/'>
+					<img
+						src='/HRLogo-removebg.png'
+						alt='HRnet Logo'
+						className='h-12 w-12'
+					/>
+				</Link>
 				<h1 className='text-2xl font-bold'>HRnet</h1>
 				<nav>
 					<Link
