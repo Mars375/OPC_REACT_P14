9ae70b3935762a14bf00ec4e0ea90452e8e5d512
Mars375 <loic_2610@hotmail.fr>
2024-06-10 19:04:33 +0200

    Enhanced JSDoc and added style transitions to Input component

--- src/components/UI/atoms/Input.tsx
+++ src/components/UI/atoms/Input.tsx
@@ -7,23 +7,24 @@ import { inputStyles } from "@/styles/inputStyles";
  * It supports different colors and sizes, and can be used in various forms throughout the application.
  *
  * @param {InputProps} props - Props for Input component
+ * @param {string} [props.color='primary'] - The color theme of the input, default is 'primary'.
+ * @param {string} [props.size='medium'] - The size of the input, default is 'medium'.
+ * @param {string} [props.className] - Additional custom classes for more specific styling.
  * @returns {React.ReactElement} A styled input element.
  */
-const Input: React.FC<InputProps> = ({
-	color = "primary",
-	size = "medium",
-	className,
-	...rest
-}) => {
-	const colorClass = inputStyles.color(color);
-	const sizeClass = inputStyles.size[size];
 
-	return (
-		<input
-			{...rest}
-			className={`${inputStyles.base} ${colorClass} ${sizeClass} ${className}`}
-		/>
-	);
-};
+const Input: React.FC<InputProps> = React.memo(
+	({ color = "primary", size = "medium", className, ...rest }) => {
+		const colorClass = inputStyles.color(color);
+		const sizeClass = inputStyles.size[size];
+
+		return (
+			<input
+				{...rest}
+				className={`${inputStyles.base} ${colorClass} ${sizeClass} ${className}`}
+			/>
+		);
+	}
+);
 
 export default Input;
