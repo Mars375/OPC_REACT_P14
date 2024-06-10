1f2aa709d3f7ae031f4c4509355bffe420f1e60c
Mars375 <loic_2610@hotmail.fr>
2024-06-10 18:00:43 +0200

    Enhance: Add documentation and refine styles for Select component

--- src/components/UI/atoms/Select.tsx
+++ src/components/UI/atoms/Select.tsx
@@ -1,6 +1,17 @@
 import React, { SelectHTMLAttributes } from "react";
-import { baseClasses, colorClasses, sizeClasses } from "@styles/inputStyles";
 
+/**
+ * Custom select component that can be styled with TailwindCSS.
+ * It supports different colors and sizes, and can be used in various forms throughout the application.
+ * This component is typically used for selecting one option from a list.
+ *
+ * @param {Object} props - Props for Select component
+ * @param {Array<{value: string; label: string}>} props.options - Options for the select dropdown.
+ * @param {'primary' | 'secondary' | 'error' | 'warning' | 'info' | 'success'} [props.color='primary'] - Color variant of the select.
+ * @param {'small' | 'medium' | 'large'} [props.size='medium'] - Size of the select.
+ * @param {string} [props.className] - Additional CSS classes for further customization.
+ * @returns {React.ReactElement} A styled select element.
+ */
 interface SelectProps
 	extends Omit<SelectHTMLAttributes<HTMLSelectElement>, "size"> {
 	options: { value: string; label: string }[];
@@ -14,17 +25,16 @@ const Select: React.FC<SelectProps> = ({
 	color = "primary",
 	size = "medium",
 	className,
-	id,
 	...rest
 }) => {
-	const colorClass = colorClasses[color];
-	const sizeClass = sizeClasses[size];
+	const colorClass = `bg-${color}-500 text-white`;
+	const sizeClass =
+		size === "large" ? "p-4" : size === "medium" ? "p-3" : "p-2";
 
 	return (
 		<select
-			id={id}
 			{...rest}
-			className={`${baseClasses} ${colorClass} ${sizeClass} w-full ${className}`}
+			className={`${colorClass} ${sizeClass} w-full rounded-md ${className}`}
 		>
 			{options.map((option) => (
 				<option key={option.value} value={option.value}>
