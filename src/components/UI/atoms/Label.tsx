43d3986364873c587beb717ce14f1c03912f8848
Mars375 <loic_2610@hotmail.fr>
2024-06-10 17:57:37 +0200

    Enhance: Add documentation and refine styles for Label component

--- src/components/UI/atoms/Label.tsx
+++ src/components/UI/atoms/Label.tsx
@@ -1,5 +1,14 @@
 import React, { LabelHTMLAttributes } from "react";
 
+/**
+ * Represents a label for form elements and other UI components.
+ * This component is used to provide accessible labels for interactive elements.
+ *
+ * @param {Object} props - Props for Label component
+ * @param {string} props.text - The text content of the label.
+ * @param {string} [props.className] - Additional CSS classes for further customization.
+ * @returns {React.ReactElement} A styled label element.
+ */
 interface LabelProps extends LabelHTMLAttributes<HTMLLabelElement> {
 	text: string;
 }
