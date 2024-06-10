d3a5a3045f483a72527fcd56e73f4308119651f1
Mars375 <loic_2610@hotmail.fr>
2024-06-10 17:21:50 +0200

    Refactor: Optimize GeneralTemplate with React.memo and enhance styling

--- src/components/templates/GeneralTemplate.tsx
+++ src/components/templates/GeneralTemplate.tsx
@@ -6,16 +6,22 @@ interface GeneralTemplateProps {
 	children: React.ReactNode;
 }
 
-const GeneralTemplate: React.FC<GeneralTemplateProps> = ({ children }) => {
-	return (
-		<div className='flex flex-col min-h-screen bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-100'>
-			<Header />
-			<main className='container mx-auto p-6 flex-grow dark:text-gray-100 flex justify-center items-center'>
-				{children}
-			</main>
-			<Footer />
-		</div>
-	);
-};
+/**
+ * A general layout template that includes a header, footer, and a main content area.
+ * This template is used throughout the application to maintain a consistent layout structure.
+ */
+const GeneralTemplate: React.FC<GeneralTemplateProps> = React.memo(
+	({ children }) => {
+		return (
+			<div className='flex flex-col min-h-screen bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-100'>
+				<Header />
+				<main className='container mx-auto p-6 flex-grow dark:text-gray-100 flex justify-center items-center'>
+					{children}
+				</main>
+				<Footer />
+			</div>
+		);
+	}
+);
 
 export default GeneralTemplate;
