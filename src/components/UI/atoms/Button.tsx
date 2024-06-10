2d63d75c427da38f767dc1306cb61976f4444a84
Mars375 <loic_2610@hotmail.fr>
2024-06-10 17:23:42 +0200

    Refactor: Enhance Button component with TailwindCSS and React.memo

--- src/components/UI/atoms/Button.tsx
+++ src/components/UI/atoms/Button.tsx
@@ -1,17 +1,34 @@
-import React, { ButtonHTMLAttributes } from "react";
-import {
-	baseClasses,
-	colorClasses,
-	variantClasses,
-	sizeClasses,
-} from "@styles/buttonStyles";
+import React from "react";
 
-interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+interface ButtonProps {
 	color?: "primary" | "secondary" | "error" | "warning" | "info" | "success";
 	variant?: "text" | "outlined" | "contained";
 	size?: "small" | "medium" | "large";
 	children: React.ReactNode;
+	className?: string;
 }
+
+const buttonStyles = {
+	base: "px-4 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-opacity-50",
+	color: {
+		primary: "bg-blue-500 text-white",
+		secondary: "bg-gray-500 text-white",
+		error: "bg-red-500 text-white",
+		warning: "bg-yellow-500 text-black",
+		info: "bg-blue-300 text-white",
+		success: "bg-green-500 text-white",
+	},
+	variant: {
+		text: "bg-transparent",
+		outlined: "border-2",
+		contained: "shadow-md",
+	},
+	size: {
+		small: "text-sm",
+		medium: "text-base",
+		large: "text-lg",
+	},
+};
 
 const Button: React.FC<ButtonProps> = React.memo(
 	({
@@ -22,14 +39,14 @@ const Button: React.FC<ButtonProps> = React.memo(
 		className,
 		...rest
 	}) => {
-		const colorClass = colorClasses[color];
-		const variantClass = variantClasses[variant];
-		const sizeClass = sizeClasses[size];
+		const colorClass = buttonStyles.color[color];
+		const variantClass = buttonStyles.variant[variant];
+		const sizeClass = buttonStyles.size[size];
 
 		return (
 			<button
 				{...rest}
-				className={`${baseClasses} ${colorClass} ${variantClass} ${sizeClass} ${className}`}
+				className={`${buttonStyles.base} ${colorClass} ${variantClass} ${sizeClass} ${className}`}
 			>
 				{children}
 			</button>
