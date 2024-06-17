4f836c25efec4e8684c438a24df25f531f7eeada
Mars375 <loic_2610@hotmail.fr>
2024-06-17 18:40:35 +0200

    Update input styles for enhanced error visibility
    - Adjusted error style in inputStyles to include red border, slightly red background, and dark red text for better error indication.
    - Maintained primary and secondary styles with white and gray backgrounds respectively.
    - Ensured dark mode compatibility with appropriate color adjustments.

--- src/styles/inputStyles.ts
+++ src/styles/inputStyles.ts
@@ -1,13 +1,11 @@
 export const inputStyles = {
 	base: "border border-gray-300 dark:border-gray-700 rounded p-2 w-full text-gray-900 dark:text-gray-100 h-12 focus:outline-none focus:ring-1 focus:ring-opacity-50",
 	color: {
-		primary: "bg-white text-gray-900 dark:bg-gray-700 dark:text-gray-100",
-		secondary:
-			"bg-secondary text-secondary-700 dark:bg-secondary dark:text-secondary-500",
-		tertiary:
-			"bg-tertiary-500 text-tertiary-700 dark:bg-tertiary-700 dark:text-tertiary-500",
+		primary: "bg-white text-gray-900 dark:bg-gray-700 dark:text-white",
+		secondary: "bg-gray-300 text-gray-900 dark:bg-gray-500 dark:text-white",
+		tertiary: "bg-green-500 text-white dark:bg-green-700 dark:text-white",
 		error:
-			"ring-1 ring-red-500 border-red-500 dark:ring-red-700 dark:border-red-700",
+			"border-red-500 bg-red-50 text-red-700 dark:bg-red-900 dark:text-red-300", // Bordure rouge avec un fond légèrement rouge et texte rouge foncé
 	},
 	size: {
 		small: "p-2 text-sm",
